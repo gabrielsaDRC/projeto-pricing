82e642f75b15259d772d76d2fa5b719fd5168fba
Gabriel Dos Santos <gsantos@stefanini.com>
2024-01-03 08:28:33 -0300

    Adição de Pesados

--- src/App.js
+++ src/App.js
@@ -9,6 +9,12 @@ const App = () => {
   const [quotes, setQuotes] = useState([]);
   const [jsonValidationMessage, setJsonValidationMessage] = useState('');
   const [statusMessages, setStatusMessages] = useState([]);
+  const [categoria, setCategoria] = useState('leves'); 
+
+  
+  const [quoteNumber, setQuoteNumber] = useState('');
+  const [quotesPesados, setQuotesPesados] = useState([]);
+  const [reviewQuotes, setReviewQuote] = useState([]);
 
   const handleParseJson = () => {
     try {
@@ -23,6 +29,24 @@ const App = () => {
       setStatusMessages([]);
     }
   };
+
+  const handleQuoteJson = async (quote) => {
+    var url = 'https://ouropricing-prd.unidas.com.br/api/price/review-heavy/quotationLines?quote=' + quote;
+
+    try {
+      const response = await axios.get(url);
+      if (response.status === 200) {
+        setQuotesPesados(response.data.data);
+      } else {
+        setQuotesPesados([]);
+        alert('Erro ao buscar dados da cotação. Verifique a saída do console.')
+      }
+    } catch (error) {
+      console.error('Erro ao obter o cotação:', error);
+      alert('Erro ao buscar dados da cotação. Certifique-se de a cotação existe.');
+      setQuotesPesados([]);
+    }
+  }
 
   const handlePrecificar = async (quote, index) => {
     try {
@@ -49,11 +73,80 @@ const App = () => {
     }
   };
 
+  const handleReviewQuote = async (id) => {
+    var url = 'https://ouropricing-prd.unidas.com.br/api/price/review-heavy/' + id;
+
+    try {
+      debugger
+      const response = await axios.get(url);
+      if (response.status === 200) {
+        setReviewQuote(response.data.data);
+        return response.data.data;
+      } else {
+        setReviewQuote([]);
+        alert('Erro ao buscar dados da revisão. Verifique a saída do console.')
+      }
+    } catch (error) {
+      console.error('Erro ao obter o cotação:', error);
+      alert('Erro ao buscar dados da revisão. Certifique-se de a cotação existe.');
+      setReviewQuote([]);
+    }
+  }
+
+  const handleExtrair = async(selectedQuote, index) => {
+    var quoteLines = [];
+    debugger
+    var reviewQuote = await handleReviewQuote(selectedQuote.priceEngineReview.id)
+    debugger;
+    var quoteLine = JSON.parse(reviewQuote.input);
+    var result = JSON.parse(reviewQuote.result);
+    
+    result.InvestimentTotal = result.AccesoriesInvestment + result.MobilizationInvestment + result.EquipmentInvestment;
+    reviewQuote.investimentTotal = result.InvestimentTotal;
+    
+    if (quoteLine.InsuranceFranchise < 0)
+    {
+      alert("A Linha de Cotação não possui valor definido para Franquia Seguro!");
+    }
+    
+    quoteLine.HasPrecification = true;
+    quoteLine.MethodCalculate = selectedQuote?.priceEngineInput?.methodCalculate;
+    quoteLine.SpreadValor = selectedQuote?.priceEngineInput?.spreadValor;
+    quoteLine.SourceApplication = quoteLine.SpreadValor <= 0 ? 1 : 2;
+
+    quoteLine.SalesDaysTerm = selectedQuote?.priceEngineInput.salesDaysTerm;
+    quoteLine.InsuranceAmount = selectedQuote?.priceEngineInput.insuranceAmount;
+    quoteLine.VehicleInsuranceAmount = selectedQuote?.priceEngineInput.vehicleInsuranceAmount;
+    quoteLine.ResponsabilityInsurance = selectedQuote?.priceEngineInput.responsabilityInsurance;
+    quoteLine.InsuranceFranchise = selectedQuote?.priceEngineInput.insuranceFranchise;
+    quoteLine.PercentRisk = selectedQuote?.priceEngineInput.percentRisk;
+    quoteLine.AliqISS = selectedQuote?.priceEngineInput.aliqISS;
+    quoteLine.DataInicioSafra = selectedQuote?.priceEngineInput.dataInicioSafra;
+    quoteLine.RentalAmount = selectedQuote?.priceEngineResult.rentalAmount;
+
+    
+    quoteLine.OldStatus = quoteLine.OldStatus == null || quoteLine.OldStatus == '' ? reviewQuote.previousStatus : quoteLine.OldStatus;
+    quoteLines.push(quoteLine)
+
+    quoteLines.map((line) => {
+      const body = {
+        Gatilho: 11,
+        QuoteLineInputs: [line],
+      };
+
+      copy(JSON.stringify(body));
+      alert('Gatilho copiado para o clipboard.');
+    });    
+    
+  }
+
   const handleLimpar = () => {
     setJsonInput('');
     setQuotes([]);
     setJsonValidationMessage('');
     setStatusMessages([]);
+    setQuotesPesados([]);
+    setQuoteNumber('');
   };
 
   const handlePadrao = () => {
@@ -70,18 +163,28 @@ const App = () => {
       <button className="default-button" onClick={handlePadrao}>
         Padrão
       </button>
-      <textarea
-        placeholder="Insira o JSON aqui"
-        value={jsonInput}
-        onChange={(e) => setJsonInput(e.target.value)}
-      />
-      <button onClick={handleParseJson}>Analisar JSON</button>
+      <select onChange={(e) => setCategoria(e.target.value)}>
+        <option value="leves">Leves</option>
+        <option value="pesados">Pesados</option>
+      </select>
+      {categoria === 'leves' ? (
+      <><textarea
+          placeholder="Insira o JSON aqui"
+          value={jsonInput}
+          onChange={(e) => setJsonInput(e.target.value)} /><button onClick={handleParseJson}>Analisar JSON</button></>
+      ) : null}
+      {categoria === 'pesados' ? (
+      <><textarea
+          placeholder="Insira a Cotação aqui"
+          value={quoteNumber}
+          onChange={(e) => setQuoteNumber(e.target.value)} /><button onClick={() => handleQuoteJson(quoteNumber)}>Extrair Cotação</button></>
+      ) : null}
       {jsonValidationMessage && (
         <div className={jsonValidationMessage.includes('sucesso') ? 'success' : 'error'}>
           {jsonValidationMessage}
         </div>
       )}
-      <div className="quote-container">
+      <div className="quote-container-leves">
         {quotes.map((quote, index) => (
           <div className="quote-item" key={quote.lineNumber}>
             <p>Linenumber: {quote.lineNumber}</p>
@@ -93,6 +196,14 @@ const App = () => {
                   {msg.message}
                 </span>
               ))}
+          </div>
+        ))}
+      </div>
+      <div className="quote-container-pesados">
+        {quotesPesados.map((quote, index) => (
+          <div className="quote-item" key={quote.priceEngineReview.lineNumber}>
+            <p>Linenumber: {quote.priceEngineReview.lineNumber}</p>
+            <button onClick={() => handleExtrair(quote, index)}>Copiar JSON</button>
           </div>
         ))}
       </div>
